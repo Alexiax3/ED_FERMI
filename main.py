31ea0c50055aaf06d6da218f98a6361fa7d5dcdd
Alexiax3 <89424285+Alexiax3@users.noreply.github.com>
2023-11-13 03:45:50 +0000

    Actividad 1

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+st.write("hello")
+st.title("Desintegración de Partículas basado en las Ecuaciones de Fermi")
+"""El modelo de desintegración de partículas basado en las ecuaciones de Fermi, conocido como la teoría de la desintegración beta permiten calcular la probabilidad de ocurrencia de la desintegración y la energía. Es un proceso en el cual un núcleo atómico experimenta una transformación al emitir una partícula beta, que puede ser un electrón o su contraparte positiva, el positrón, acompañado por la emisión de un neutrino o un antineutrino.
+
+Estas ecuaciones son altamente relevantes en situaciones diversas, desde la operación de reactores nucleares hasta la interpretación de los procesos de desintegración radiactiva en cualquier lugar en el que se encuentren partículas subatómicas.  
+"""
---
+++ pages/Definiones.py
@@ -0,0 +1,13 @@
+import streamlit as st
+st.title("Definiciones")
+"""                 El proceso se escribe esquemáticamente donde X e Y son la especie nuclear inicial y final, y C es el antineutrino. Porque los neutrinos y antineutrinos tienen una probabilidad de interacción extremadamente pequeña con la materia,son indetectables para todos los fines prácticos. El núcleo de retroceso Y aparece con un pequeña energía de retroceso, que normalmente está por debajo del umbral de ionización, y por lo tanto no se puede detectar por medios convencionales. Por lo tanto, la única radiación ionizante significativa producida por la desintegración beta es el electrón rápido o partícula beta en sí.
+
+Porque la mayoría de los radionucleidos producidos por el bombardeo de neutrones de materiales estables son beta-activo, una gran variedad de emisores beta son fácilmente disponibles a través de la producción en un flujo de reactor. Se pueden obtener especies con muchas vidas medias diferentes, que van desde miles de años hasta una vida media tan corta como sea práctico en la aplicación. La mayoría de las desintegraciones beta poblar un estado excitado del núcleo del producto, de modo que la desexcitación posterior los rayos gamma se emiten junto con las partículas beta en muchas fuentes beta comunes. 
+
+Cada transición de desintegración beta específica se caracteriza por una energía de desintegración fija o valor Q.
+
+Debido a que la energía del núcleo de retroceso es prácticamente cero, esta energía se comparte entre el partícula beta y el neutrino "invisible". La partícula beta aparece así con una energía que varía de decaimiento en decaimiento y puede variar de cero a la "energía del punto final beta",
+
+El valor Q para un decaimiento dado se cita normalmente asumiendo que la transición tiene lugar entre los estados fundamentales de los núcleos padre e hija. Si la transición implica un estado excitado del padre o de la hija, la energía del punto final del espectro beta correspondiente será cambiado por la diferencia en las energías de excitación.
+
+Dado que varios estados excitados pueden ser poblados en algunos esquemas de desintegración, el espectro de partículas beta medido puede consistir en varios componentes con diferentes energías de punto final. """
